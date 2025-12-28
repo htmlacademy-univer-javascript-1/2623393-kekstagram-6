f142de88b07758b99431857dfe650ec6618a7b9d
Erkenn <erkenkulbusinov@gmail.com>
2025-12-28 18:06:16 +0500

    12-1

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,73 @@
 import { renderThumbnails } from './thumbnail-render.js';
 import { openFullPicture } from './full-picture.js';
+import { debounce } from './util.js';
 
-const initGallery = (picturesData) => {
+let currentPictures = [];
+
+export const initGallery = (picturesData) => {
+  currentPictures = [...picturesData];
   renderThumbnails(picturesData);
 
   const thumbnails = document.querySelectorAll('.picture');
-
   thumbnails.forEach((thumbnail, index) => {
     thumbnail.addEventListener('click', (evt) => {
       evt.preventDefault();
-
-      const pictureData = picturesData[index];
-
-      openFullPicture(pictureData);
+      openFullPicture(picturesData[index]);
     });
   });
 };
 
-export { initGallery };
+export const showFilters = () => {
+  const filtersElement = document.querySelector('.img-filters');
+  if (filtersElement) {
+    filtersElement.classList.remove('img-filters--inactive');
+  }
+};
+
+const getFilteredPictures = (filterType) => {
+  switch (filterType) {
+    case 'random': {
+      const shuffled = [...currentPictures].sort(() => 0.5 - Math.random());
+      return shuffled.slice(0, 10);
+    }
+
+    case 'discussed': {
+      return [...currentPictures].sort((a, b) => b.comments.length - a.comments.length);
+    }
+
+    case 'default':
+    default:
+      return [...currentPictures];
+  }
+};
+
+const applyFilter = (filterType) => {
+  const filtered = getFilteredPictures(filterType);
+  renderThumbnails(filtered);
+
+  const buttons = document.querySelectorAll('.img-filters__button');
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filterType) {
+      button.classList.add('img-filters__button--active');
+    } else {
+      button.classList.remove('img-filters__button--active');
+    }
+  });
+};
+const debouncedApplyFilter = debounce((filterType) => {
+  applyFilter(filterType);
+}, 500);
+
+export const initFilters = () => {
+  const buttons = document.querySelectorAll('.img-filters__button');
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const filterType = button.dataset.filter;
+      debouncedApplyFilter(filterType);
+    });
+  });
+
+  // Устанавливаем начальный фильтр
+  applyFilter('default');
+};
--- js/util.js
+++ js/util.js
@@ -9,3 +9,22 @@ export const createIdGen = () => {
     return lastId;
   };
 };
+
+export const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export const throttle = (callback, delayBetweenFrames) => {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+};
